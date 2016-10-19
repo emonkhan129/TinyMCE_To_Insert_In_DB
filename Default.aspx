9461bdedaf76f5c75a8566d92aadf654b98e21b2
emonkhan129 <emonkhan129@yahoo.com>
2016-10-19 15:50:06 +0600

    new

---
+++ Default.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page ValidateRequest="false" Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+     <script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
+  <script>tinymce.init({ selector: 'textarea' });</script>
+</head>
+<body>
+     <form id="form1" runat="server">
+    <div>
+        <asp:Label runat="server" ID="label1" Text="Description"></asp:Label>
+        <textarea ID="descriptionTextbox" runat="server">Easy! You should check out MoxieManager!</textarea>
+       <%--<asp:TextBox runat="server" TextMode="MultiLine" ID="dexcriptionTextbox"></asp:TextBox><br/>--%>
+        <asp:Button runat="server" ID="saveButton" Text="Save" OnClick="saveButton_Click"/>
+        <asp:Label ID="Label2" runat="server"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    SqlConnection con = new SqlConnection(@"Data Source = LAB-301-30\SQLEXPRESS; Initial Catalog=TinyMceTestDB; Integrated Security=true");
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void saveButton_Click(object sender, EventArgs e)
+    {
+        string description = descriptionTextbox.InnerHtml;
+        string query = "insert into tinyMCE (description) values('" + description + "')";
+        SqlCommand cmd = new SqlCommand(query,con);
+        con.Open();
+
+        int count = cmd.ExecuteNonQuery();
+        if (count > 0)
+        {
+            label1.Text = "saved";
+        }
+        else
+        {
+            label1.Text = "not saved";
+        }
+        con.Close();
+    }
+}
